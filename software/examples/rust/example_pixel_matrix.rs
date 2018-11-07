580b05cc10f3c535cdfd65cbb800d41dd02986d1
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:25:24 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_pixel_matrix.rs
+++ software/examples/rust/example_pixel_matrix.rs
@@ -1,6 +1,6 @@
 use std::{error::Error, io};
 
-use tinkerforge::{ipconnection::IpConnection, oled_64x48_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, oled_64x48_bricklet::*};
 
 const HOST: &str = "127.0.0.1";
 const PORT: u16 = 4223;
@@ -8,7 +8,7 @@ const UID: &str = "XYZ"; // Change XYZ to the UID of your OLED 64x48 Bricklet
 const WIDTH: usize = 64;
 const HEIGHT: usize = 48;
 
-fn draw_matrix(oled: &OLED64x48Bricklet, pixels: [[bool; WIDTH]; HEIGHT]) {
+fn draw_matrix(oled: &Oled64x48Bricklet, pixels: [[bool; WIDTH]; HEIGHT]) {
     let mut pages = [[0u8; WIDTH]; HEIGHT / 8];
     for (col_idx, col) in pages.iter_mut().enumerate() {
         for (row_idx, byte) in col.iter_mut().enumerate() {
@@ -29,13 +29,13 @@ fn draw_matrix(oled: &OLED64x48Bricklet, pixels: [[bool; WIDTH]; HEIGHT]) {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ipcon = IpConnection::new(); // Create IP connection
-    let oled_64x48_bricklet = OLED64x48Bricklet::new(UID, &ipcon); // Create device object
+    let oled = Oled64x48Bricklet::new(UID, &ipcon); // Create device object
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd
+                                          // Don't use device before ipcon is connected
 
     // Clear display
-    oled_64x48_bricklet.clear_display();
+    oled.clear_display();
 
     // Draw checkerboard pattern
     let mut pixels = [[false; WIDTH]; HEIGHT];
@@ -45,7 +45,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    draw_matrix(&oled_64x48_bricklet, pixels);
+    draw_matrix(&oled, pixels);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
